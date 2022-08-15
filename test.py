db05659fc102f13cc4d626e2b7a63affcf9c4ffc
piology-1 <pius.grossmann@web.de>
2022-08-15 12:44:10 +0200

    added songs

--- test.py
+++ test.py
@@ -8,7 +8,8 @@ import os
 # getting the EMVs
 client_id = os.environ.get('client_id','client_id not found')
 client_secret = os.environ.get('client_secret','client_secret not found')
-
+# print(client_id)
+# print(client_secret)
 
 scopes = 'ugc-image-upload, user-modify-playback-state, user-follow-modify, user-read-recently-played,user-read-playback-position, playlist-read-collaborative, app-remote-control, user-read-playback-state, user-read-email, streaming user-top-read, playlist-modify-public, user-library-modify, user-follow-read, user-read-currently-playing, user-library-read, playlist-read-private, user-read-private, playlist-modify-private'
 
@@ -31,7 +32,9 @@ sp = spotipy.Spotify(auth_manager=auth_manage)
 favorite_songs = sp.current_user_saved_tracks()
 #rec_played = sp.current_user_recently_played()
 #saved_episodes = sp.current_user_saved_episodes()
-#data = sp.categories()
+data = sp.categories()
+
+pprint(favorite_songs)
 
 url = favorite_songs['href']
 # Making a GET request
