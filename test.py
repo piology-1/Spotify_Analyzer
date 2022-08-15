9d3780ce1fdbe262d71759c67b62e200e37251aa
piology <grossmann.pius-it20@it.dhbw-ravensburg.de>
2022-08-15 09:27:22 +0200

    comment unnecessary code out

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ import spotipy.util as util
 from spotipy.oauth2 import SpotifyOAuth
 from prettyprinter import pprint
 import requests
-   
-
 
 client_id = "92cce333b3da417a95d8ca738477b241"
 client_Secret = "7bbf053307e54547be82030359eb072c"
@@ -20,10 +18,10 @@ auth_manage = SpotifyOAuth(client_id=client_id, client_secret=client_Secret,
 token = auth_manage.get_access_token(code='code')
 # pprint(token)
 sp = spotipy.Spotify(auth=token['access_token'])
-#auth_manager=auth_manage, 
+# auth_manager=auth_manage,
 
 # https://accounts.spotify.com/authorize
- 
+
 favorite_songs = sp.current_user_saved_tracks()
 #rec_played = sp.current_user_recently_played()
 #saved_episodes = sp.current_user_saved_episodes()
