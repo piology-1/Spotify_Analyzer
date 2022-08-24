e5396e9af1ad474d2ef0f3796a6c8339518d3d59
piology-1 <pius.grossmann@web.de>
2022-08-24 17:15:16 +0200

    extended info Tab with Layout Management

--- PySide6_GUI.py
+++ PySide6_GUI.py
@@ -1,17 +1,18 @@
-from ast import main
+from ctypes import alignment
+from random import triangular
+from tkinter import CENTER, TOP
 from utils import *
 from authentication import authenticate
 import requests
 from io import BytesIO
-from PIL import Image, ImageTk
 from prettyprinter import pprint
 
 ''' --- GUI Imports --- '''
 import sys
+from PIL import Image, ImageTk
 from PySide6 import *
-from PySide6.QtWidgets import *
+from PySide6.QtWidgets import *  # z.B. PySide6.QtWidgets.QVBoxLayout
 from PySide6.QtGui import *  # QFont
-
 
 ''' --- constants --- '''
 WIN_WIDTH, WIN_HEIGHT = 1100, 600  # default window geometry
@@ -53,17 +54,39 @@ class InfoTab(QWidget):
     def __init__(self, parent: QWidget):
         super().__init__(parent)
 
+        self.setStyleSheet(
+            "background-image: url(imgs/Spotify_color_gradient.png)")
         data = get_current_user(sp=sp)
         user = data["name"]
-        welcome_text_Label = QLabel(f"Hello, nice to see you!")
+        welcome_text_Label = QLabel(f"Hello {user}!", self)
+        welcome_text_Label.setFont(QFont("Helvetica", 40))
+        welcome_text_Label.setStyleSheet(
+            "font-weight: bold; background: transparent")
 
-        info_text = f"Hello,\nI'm glad, that you're interested in this Spotify Analyzer.\nThis Application will show you:\n\t- Your all time favorite Tracks\n\t- Your all time favorite Artists\n\t- Your all recently played Songs\n\t- The possibility, to add Songs to the queue\n\nHave fun and enjoy\n\t~Piology"
-        info_Label = QLabel(info_text)
+        info_text = f"I'm glad, that you're interested in this Spotify Analyzer.\nThis Application will show you:\n\t- Your all time favorite Tracks\n\t- Your all time favorite Artists\n\t- Your all recently played Songs\n\t- The possibility, to add Songs to the queue\n\nHave fun and enjoy\n\t~Piology"
+        info_Label = QLabel(info_text, self)
+        info_Label.setStyleSheet("background: transparent")
 
-        main_layout = QHBoxLayout()
-        main_layout.addWidget(welcome_text_Label)
-        main_layout.addWidget(info_Label)
-        main_layout.addStretch(1)
+        # downloading the profile pic
+        user_picture_url = data['profile_img']
+        user_picture = QImage()
+        user_picture.loadFromData(requests.get(url=user_picture_url).content)
+        picture_resized = user_picture.scaled(600, 600)
+        image_label = QLabel(self)
+        image_label.setPixmap(QPixmap(user_picture))
+        image_label.show()
+        image_label.setStyleSheet("background: transparent")
+        # TODO: cloud make the profile picture Circular (https://www.geeksforgeeks.org/pyqt5-how-to-create-circular-image-from-any-image/)
+
+        # main_layout.addRow(image_label)
+        # QFormLayout(self) with addRow(QLabel(), QLineEdit()) provides an entry Box to the QLabel
+
+        main_layout = QGridLayout(self)
+        # main_layout.addWidget(QWidget, row, column, rowSpan, columnSpan, alignment)
+        main_layout.addWidget(welcome_text_Label, 0, 0, 1, 2, Qt.AlignCenter)
+        main_layout.addWidget(info_Label, 1, 0, Qt.AlignTop)
+        main_layout.addWidget(image_label, 1, 1, Qt.AlignCenter)
+        # main_layout.addStretch(1)
         self.setLayout(main_layout)
 
 
@@ -75,7 +98,7 @@ class MainWindow(QMainWindow):   # It dosn't really work with QMainWindow
     def __init__(self, parent: QWidget = None):
         super(MainWindow, self).__init__(parent)
 
-        ''' init geoemtry '''
+        ''' init geoemetry '''
         self.setWindowTitle("Spotify Analyzer")
         # self.setGeometry(50, 50, WIN_WIDTH, WIN_HEIGHT)
         spotify_logo = QIcon("imgs/Spotify_logo.png")
@@ -84,21 +107,52 @@ class MainWindow(QMainWindow):   # It dosn't really work with QMainWindow
 
         ''' ------------------------------------------------------------------ '''
 
-        tab_widget = QTabWidget()  # creating a Tabwidget
+        self.tab_widget = QTabWidget()  # creating a Tabwidget
+
         # setting the tabWidget as the main window’s central widget
-        self.setCentralWidget(tab_widget)
+        self.setCentralWidget(self.tab_widget)
 
-        #  all Tabs to the TabWidget
-        tab_widget.addTab(InfoTab(self), QIcon(
+        # TODO: Do I need a QTabWidget() and a QTabBar()?
+        #tab_bar = QTabBar()
+        #tab_bar = all_tabs.tabBar()
+        # tab_bar = all_tabs.tabBar() # is this eequal to tab_bar = QTabBar()?
+        tab_bar = self.tab_widget.tabBar()
+
+        #  adding all Tabs to the TabWidget
+        self.tab_widget.addTab(InfoTab(self), QIcon(
             "imgs/info_icon.png"), "General Infos")
-        tab_widget.addTab(TopSongsTab(self), "Your all Time Fav's")
-        tab_widget.addTab(TopArtistsTab(self), "Your favorite Artists")
+        self.tab_widget.addTab(TopSongsTab(self), "Your all Time Fav's")
+        self.tab_widget.addTab(TopArtistsTab(self), "Your favorite Artists")
 
-        main_layout = QVBoxLayout()
-        main_layout.addWidget(tab_widget)
+        self.tab_widget.setMovable(True)
+
+        self.tab_widget.setTabsClosable(True)
+        # self.tab_widget.tabCloseRequested(self.close_Tab)
+
+        self.tab_widget.setTabShape(QTabWidget.TabShape.Triangular)
+        self.tab_widget.setTabPosition(QTabWidget.North)
+        self.tab_widget.setUsesScrollButtons(True)
+
+        tab_bar.setStyleSheet(
+            f"background: {SPOTIFY_GREEN}; color: white; border: 5px")
+        self.tab_widget.setStyleSheet(
+            "background-image: url(imgs/Spotify_color_gradient.png)")
+
+        # creating a layout for flexible usage
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(self.tab_widget)
         self.setLayout(main_layout)
 
         self.show()
+
+    def close_Tab(self, current_Index):
+        """
+            callback Function, when the Tabs getting closed
+            TODO: adding functionality, to add the tabs again (maybe from the Info Tab?)
+            TODO: Making the Info Tab uncloseable
+
+        """
+        self.tab_widget.removeTab(current_Index)  # removes the current Tab
 
 
 app = QApplication(sys.argv)
